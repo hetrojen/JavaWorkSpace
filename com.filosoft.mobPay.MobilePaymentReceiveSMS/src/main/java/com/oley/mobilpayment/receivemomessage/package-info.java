008382cfb3d6e124aa75cf95ad4feae15673d819
serkan <serkan@DESKTOP-M55T79F>
2016-04-26 13:58:26 +0300

    initial commit

---
+++ com.filosoft.mobPay.MobilePaymentReceiveSMS/src/main/java/com/oley/mobilpayment/receivemomessage/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://cservices.mikro-odeme.com.tr/mapping/generated", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.oley.mobilpayment.receivemomessage;
